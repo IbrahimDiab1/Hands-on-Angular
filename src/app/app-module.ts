c3c1363d15f964b029008162ad3df8c62c470f64
Ibrahim Diab <ibrahim0diab@gmail.com>
2025-06-02 15:14:35 +0300

    v1

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,19 +1,24 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './navbar/navbar';
+import { Products } from './products/products';
+import { Footer } from './footer/footer';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Navbar,
+    Products,
+    Footer
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
+    AppRoutingModule,
+    FormsModule
   ],
   bootstrap: [App]
 })
